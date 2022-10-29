7ac745ee587806867af6f5207ed73466235d6ebd
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-10-29 16:53:42 +0200

    subscribing to the store

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -37,6 +37,15 @@ export class ProductListComponent implements OnInit, OnDestroy {
       next: (products: Product[]) => this.products = products,
       error: err => this.errorMessage = err
     });
+
+    // TODO: Unsubscribe
+    this.store.select('products').subscribe(
+      products => {
+        if (products) {
+          this.displayCode = products.showProductCode;
+        }
+      }
+    )
   }
 
   ngOnDestroy(): void {
