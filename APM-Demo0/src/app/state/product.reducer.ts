1797d9e1bf652a8ecf6ff5ddce5b14dc8d5ef5fb
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-10-29 16:54:24 +0200

    don't need logging anymore

--- APM-Demo0/src/app/state/product.reducer.ts
+++ APM-Demo0/src/app/state/product.reducer.ts
@@ -3,7 +3,6 @@ import { createAction, on, createReducer } from "@ngrx/store";
 export const productReducer = createReducer(
   { showProductCode: true },
   on(createAction('[PRODUCT] Toggle Product Code'), state => {
-    console.log('original state: ' + JSON.stringify(state));
     return {
       ...state,
       showProductCode: !state.showProductCode
