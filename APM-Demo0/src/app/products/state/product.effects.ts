fa12c7ce475217393c1a00a87673fda39a70ad21
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-10-31 18:01:39 +0100

    Building the Effect

--- APM-Demo0/src/app/products/state/product.effects.ts
+++ APM-Demo0/src/app/products/state/product.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { ProductService } from '../product.service';
 import * as ProductActions from './product.actions'
-import { map, mergeMap, catchError } from 'rxjs/operators'
+import { map, mergeMap, catchError, concatMap } from 'rxjs/operators'
 import { of } from 'rxjs'
 
 @Injectable()
@@ -20,5 +20,19 @@ export class ProductEffects {
         catchError(error => of(ProductActions.loadProductsFailure( {error})))
       ))
     )
+  })
+
+  updateProduct$ = createEffect(() => {
+    return this.actions$
+      .pipe(
+        ofType(ProductActions.updateProduct),
+        concatMap(action => 
+          this.productService.updateProduct(action.product)
+            .pipe(
+              map(product => ProductActions.updateProductSuccess({ product })),
+              catchError(error => of(ProductActions.updateProductFailure({ error })))
+            )
+        )
+      )
   })
 }
