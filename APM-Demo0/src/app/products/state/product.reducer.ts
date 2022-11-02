b0d3e5c8514a8fdddd95c625b13bb7b9d023ac95
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-11-02 21:47:14 +0100

    add Success/Failure actions

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -109,5 +109,22 @@ export const productReducer = createReducer<ProductState>(
       products: [],
       error: action.error
     }
+  }),
+  on(ProductActions.updateProductSuccess, (state, action): ProductState => {
+    const updatedProducts = state.products.map(
+      item => action.product.id === item.id ? action.product : item
+    );
+    return {
+      ...state,
+      products: updatedProducts,
+      currentProductId: action.product.id,
+      error: ''
+    }
+  }),
+  on(ProductActions.updateProductFailure, (state, action): ProductState => {
+    return {
+      ...state,
+      error: action.error
+    }
   })
 );
